6ba48c512be7886e54b4879e134027e5b8701077
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-02-15 11:57:34 +0500

    Create a simple HTTP-Server respond Hello World

--- HTTP-Server.go
+++ HTTP-Server.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+//Create handler struct
+type HttpHandler struct{}
+
+//Implement `ServerHTTP` method on 'HttpHandler' struct
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+	//Create response binary data
+	data := []byte("Hello World!") //slice of bytes
+
+	//Write `data` to response
+	res.Write(data)
+	fmt.Print(*req)
+}
+
+func main() {
+	//Create a new handler
+	handler := HttpHandler{}
+
+	//listen and serve
+	http.ListenAndServe(":80", handler)
 }
