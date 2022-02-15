75acb3036387661e24ac8953ac51f3c042b080c9
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-02-15 13:33:04 +0500

    We can pass nil as the value of handler, It's equal http.DefaultServeMux which is a global ServeMux instance.

--- HTTP-Server.go
+++ HTTP-Server.go
@@ -9,19 +9,18 @@ import (
 //ServeMux can accept a function for a specific route and when incoming request URL matches that route, that fubctuib will be executed.
 
 func main() {
-	//Create a new `ServeMux`
-	mux := http.NewServeMux()
+	// We can pass nil as the value of handler, It's equal http.DefaultServeMux which is a global ServeMux instance.
 
-	//handle `/` route
-	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+	//handle `/` route to `http.DefaultServerMux`
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "Hello World!")
 	})
 
-	//handle '/hello/golang' route
-	mux.HandleFunc("/hello/golang", func(res http.ResponseWriter, req *http.Request) {
+	//handle '/hello/golang' route to `http.DefaultServerMux`
+	http.HandleFunc("/hello/golang", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "Hello Golang!")
 	})
 
-	//Listen and serve using `ServeMux`
-	http.ListenAndServe(":80", mux)
+	//Listen and serve using `http.DefaultServerMux`
+	http.ListenAndServe(":80", nil)
 }
