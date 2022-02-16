e915075d60320562608d662f7d68721086a76d84
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-02-16 15:56:53 +0500

    Add server works code

--- Server/server.go
+++ Server/server.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+//Create handler structure without fields
+type HttpHandler struct {
+}
+
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func main() {
+	//Create a new `ServeMux`
+	//ServeMux can accept a function for a specific route and when incoming request
+	//URL mathces that route, that will be executed
+	mux := http.NewServeMux()
+
+	//Handle `/` route
+	//res - respond to client
+	//req - client requst
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello World!"))
+	})
+
+	//Handle '/reg'
+	mux.HandleFunc("/reg", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Welcome to registration!"))
+	})
+
+	//Start server
+	http.ListenAndServe(":80", mux)
 }
