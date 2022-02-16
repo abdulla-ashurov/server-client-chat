65750d80e5d04dfbd4c9a9e889ea56967241034c
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-02-16 17:50:00 +0500

    create functions instead lambda functions

--- Server/server.go
+++ Server/server.go
@@ -21,50 +21,56 @@ type RegUser struct {
 	Username string `json:"username"` // In json file we'll use variable name -> Username
 }
 
+//function registration
+func reg(res http.ResponseWriter, req *http.Request) {
+	//Create user type of RegUser
+	var user RegUser
+
+	//Read Json file body and write to user structure
+	err := json.NewDecoder(req.Body).Decode(&user)
+
+	//Check Decode
+	if err != nil {
+		res.Write([]byte("Server: We didn't read your JSON file. Try later..."))
+		return
+	}
+
+	//@TODO user maximum one time registration, add same users in map -> error
+
+	//Respond message to user
+	msg := "Welcome, " + user.Username + "!"
+	res.Write([]byte(msg))
+
+	//Save a new user in Map
+	users[user.Username] = id
+	id++
+}
+
+//Get all users
+func all(res http.ResponseWriter, req *http.Request) {
+	//Print all users in map
+	res.Write([]byte("\n"))
+	for name := range users {
+		res.Write([]byte(name + "\n"))
+	}
+}
+
 func main() {
 	//Create a new `ServeMux`
 	//ServeMux can accept a function for a specific route and when incoming request
 	//URL mathces that route, that will be executed
 	mux := http.NewServeMux()
 
-	//Handle `/` route
-	//res - respond to client
-	//req - client requst
+	//Handle `/` route, where -> res - respond to clien, req - client request
 	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		res.Write([]byte("Hello World!"))
 	})
 
 	//Handle '/reg'
-	mux.HandleFunc("/reg", func(res http.ResponseWriter, req *http.Request) {
-		//Create user type of RegUser
-		var user RegUser
-
-		//Read Json file body and write to user structure
-		err := json.NewDecoder(req.Body).Decode(&user)
-
-		//Check Decode
-		if err != nil {
-			res.Write([]byte("Server: We didn't read your JSON file. Try later..."))
-			return
-		}
-
-		//Respond message to user
-		msg := "Welcome, " + user.Username + "!"
-		res.Write([]byte(msg))
-
-		//Save a new user in Map
-		users[user.Username] = id
-		id++
-	})
+	mux.HandleFunc("/reg", reg)
 
 	//Handle '/all'
-	mux.HandleFunc("/all", func(res http.ResponseWriter, req *http.Request) {
-		//Print all users in map
-		res.Write([]byte("\n"))
-		for name := range users {
-			res.Write([]byte(name + "\n"))
-		}
-	})
+	mux.HandleFunc("/all", all)
 
 	//Start server
 	http.ListenAndServe(":80", mux)
