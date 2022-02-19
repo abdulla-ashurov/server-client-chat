40a83a5bbe8f3192c9e77a3f55e589abf82b2670
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-02-19 19:09:52 +0500

    add test get all registrate users

--- Server/server_test.go
+++ Server/server_test.go
@@ -7,6 +7,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type GetUserMessages struct {
+	SendMsg []server.SendUser
+	Correct []string
+}
+
 //Checking Unregistrate Users to sign up
 func TestUnregistrateUsers(t *testing.T) {
 
@@ -107,6 +112,7 @@ func TestSendMessageToRegistreteUsers(t *testing.T) {
 
 //Checking send messages to Unregistrated users
 func TestSendMessageToUnRegistrateUsers(t *testing.T) {
+
 	//Tests
 	tests := []server.SendUser{
 		{"Abdulla", "Nobody", "Hi! How are you?"},
@@ -122,4 +128,59 @@ func TestSendMessageToUnRegistrateUsers(t *testing.T) {
 	for _, value := range tests {
 		assert.EqualValues(t, server.SaveUserMessage(&value), false, "INCORRECT!")
 	}
+}
+
+//Checking get registrated user messages
+func TestGetRegistratedUserMessages(t *testing.T) {
+
+	//Registrate users
+	users := []server.User{
+		{Username: "Tom"},
+		{Username: "John"},
+		{Username: "Alfred"},
+	}
+
+	//Registrate new users
+	for _, user := range users {
+		server.CheckRegistration(user.Username)
+	}
+
+	//Tests
+	tests := []GetUserMessages{
+		{
+			SendMsg: []server.SendUser{
+				{"Tom", "John", "Hi! How are you?"},
+				{"Alfred", "John", "Me too!"}},
+			Correct: []string{
+				"Tom: Hi! How are you?",
+				"Alfred: Me too!"},
+		},
+		{
+			SendMsg: []server.SendUser{
+				{"John", "Tom", "Hi! How are you?"},
+				{"Alfred", "Tom", "Me too!"}},
+			Correct: []string{
+				"John: Hi! How are you?",
+				"Alfred: Me too!"},
+		},
+	}
+
+	//Save Users messages
+	for i := 0; i < len(tests); i++ {
+		for _, user := range tests[i].SendMsg {
+			server.SaveUserMessage(&user)
+		}
+	}
+
+	for i := 0; i < len(tests); i++ {
+
+		correct := ""
+		for j := 0; j < len(tests[i].Correct[i]); j++ {
+			correct += tests[i].Correct[j] + "\n"
+		}
+
+		//Check tests
+		assert.Equal(t, server.GetUserMessages(tests[i].SendMsg[0].Reciever), correct, "INCORRECT!")
+	}
+
 }
